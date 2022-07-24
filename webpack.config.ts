17ff8f23df1985672505109c1ab0b9a55e578031
DaCodeManiaK <jla.webprojet@gmail.com>
2022-07-24 09:25:30 +0200

    Add webpack and jest config files

---
+++ webpack.config.ts
@@ -0,0 +1,42 @@
+import path from 'path'
+import { Configuration } from 'webpack'
+import * as webpackDevServer from 'webpack-dev-server'
+
+// Set the webpack configuration
+const config: Configuration  = {
+    entry: './src/main.ts',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-typescript'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.ts',
+            '.js'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 4000
+    }
+}
+
+export default config
