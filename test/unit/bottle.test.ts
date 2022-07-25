8e733ab417f9e474a4bf678903c2264c16fb70cb
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-07-25 13:29:43 +0200

    Add property contenance test

--- test/unit/bottle.test.ts
+++ test/unit/bottle.test.ts
@@ -1,17 +1,19 @@
 import { Bottle } from './../../src/bottle'
 
 describe(`Bottle class`, () => {
-    const contenance = 1.5
-    const myBottle = new Bottle()
-    myBottle.contenance = contenance
+
+
 
     test(
         `Bottle should be instanciated`, () => {
+            const myBottle = new Bottle()
             expect(myBottle).toBeInstanceOf(Bottle)
         }
     )
 
     test(`Bottle contenance should be 1.5`, () => {
+        const myBottle = new Bottle()
+        myBottle.contenance = 1.5
         expect(myBottle.contenance).toBe(1.5)
     })
 })
