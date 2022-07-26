168b242d0f87de59f619362c8b1c875b2ff9afea
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-07-26 12:23:27 +0200

    Bottle class and tests

--- src/bottle.ts
+++ src/bottle.ts
@@ -1,5 +1,14 @@
 export class Bottle {
-    public contenance: number
+    /**
+     * Contenance must be a value in the following :
+     * 0.25 | 0.33 | 0.5 | 0.75 | 1 | 1.5 | 2 | 5
+     */
+    private contenance: number
+
+    private readonly contenances: number[] = [
+        0.25, 0.33, 0.5, 0.75, 1, 1.5, 2, 5
+    ]
+
     public packaging: string
     public closingDevice: string
     public isOpen: boolean
@@ -9,5 +18,25 @@ export class Bottle {
     }
     public close(): void {
         this.isOpen = false
+    }
+
+
+
+    public setContenance(contenance: number): void {
+        if( contenance >= 0.25 && contenance <= 5 ) {
+            if ( 
+                this.contenances.indexOf(contenance) >= 0
+            ) {
+               this.contenance = contenance 
+            }
+        }
+    }
+
+    public getContenance(): number {
+        return this.contenance
+    }
+
+    public getContenances(): number[] {
+        return this.contenances
     }
 }
--- test/unit/bottle.test.ts
+++ test/unit/bottle.test.ts
@@ -2,6 +2,8 @@ import { Bottle } from './../../src/bottle'
 
 describe(`Bottle class`, () => {
 
+    const testBottle: Bottle = new Bottle()
+
 
 
     test(
@@ -12,9 +14,25 @@ describe(`Bottle class`, () => {
     )
 
     test(`Bottle contenance should be 1.5`, () => {
-        const myBottle = new Bottle()
-        myBottle.contenance = 1000
-        expect(myBottle.contenance).toBe(1000)
+        const myBottle: Bottle = new Bottle()
+        myBottle.setContenance(1.5)
+        expect(myBottle.getContenance()).toBe(1.5)
+    })
+
+    test(`contenance attribute must be undefined if value of param passed to setContenance() is not between 0.25 and 5`, () => {
+        const myBottle: Bottle = new Bottle()
+        myBottle.setContenance(0.15)
+        expect(myBottle.getContenance()).toBeUndefined()
+
+        myBottle.setContenance(5.1)
+        expect(myBottle.getContenance()).toBeUndefined()
+    })
+
+    test(`Contenances values in the scope are valid`, () => {
+        for (const contenanceValue of testBottle.getContenances()) {
+            testBottle.setContenance(contenanceValue)
+            expect(testBottle.getContenance()).toBe(contenanceValue)
+        }
     })
 })
 
