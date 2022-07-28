6b08c5929c39ff210fa486f875f5006b62f6d368
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-07-28 11:47:10 +0200

    Make class extends Collection

--- src/intern-collection.ts
+++ src/intern-collection.ts
@@ -1,47 +1,8 @@
+import { Collection } from './collection/collection'
 import { Intern } from './intern'
 
-export class InternCollection {
-    private interns: Array<Intern>
-    public size: number
+export class InternCollection extends Collection {
+   
+    public size: number = 0
 
-    public constructor() {
-        this.interns = new Array<Intern>()
-        this.size = 0
-    }
-
-    public addIntern(intern: Intern): void {
-        let alreadyExists = false
-
-        for (const existingIntern of this.interns) {
-            if (existingIntern.id === intern.id) {
-                alreadyExists = true
-                break
-            }
-        }
-
-        if (alreadyExists === false) {
-            this.interns.push(intern)
-            this.size = this.size + 1
-        }
-    }
-
-    public removeIntern(intern: Intern): void {
-        this.size = this.size - 1
-    }
-
-    public updateIntern(intern: Intern): void {}
-
-    public getInterns(): Intern[] {
-        return this.interns
-    }
 }
-
-/**
- * Challenge :
- *  Ajouter un attribut public "size" de type number
- *  Dans la classe InternCollection
- *  Cet attribut stockera le nombre d'éléments de l'attribut "interns"
- *  de cette classe.
- *  /!\ Pensez à incrémenter l'attribut size au bon moment !
- *  /!\ Pensez à initialiser l'attribut size au bon moment !
- */
